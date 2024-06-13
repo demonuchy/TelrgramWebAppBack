9e857be66c5fd02c537c4920105a90d96920a105
demonuchy <demonuchy@gmail.com>
2024-06-13 12:10:04 +0300

    fwerf

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 require("dotenv").config();
-const { Bot, InlineKeyboard } = require("grammy");
+const { Bot, InlineKeyboard, HttpError, GrammyError } = require("grammy");
 const bot = new Bot(process.env.TOKEN_BOT);
 
-bot.on("message", async (ctx) => {
-  await ctx.reply("gergher");
+bot.command("start", async (ctx) => {
+  await ctx.reply(`Hello, ${ctx.chat.first_name}`);
 });
+
+bot.on("message", async (ctx) => {
+  const websiteButton = new InlineKeyboard();
+  await ctx.reply("You can go to our website", {
+    reply_markup: websiteButton.webApp(
+      "go to the website",
+      "https://telegramwebappfront.netlify.app/"
+    ),
+  });
+});
+
+/*bot.catch((error) => {
+  const ctx = error.ctx;
+  console.error(ctx.update.update_id);
+  const e = error.error;
+  if (e instanceof HttpError) {
+    console.error(`Error connect Telegram: ${e.description}`);
+  } else if (e instanceof GrammyError) {
+    console.error(`Error in reqest: ${e.description}`);
+  } else {
+    console.error(`Unknown error ${e}`);
+  }
+});*/
 
 bot.start();
